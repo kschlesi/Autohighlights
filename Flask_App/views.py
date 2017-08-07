d01b26fe75d4cfcd01b594d5ba72f8bab85ecade
Kimberly Schlesinger <kschlesi42@gmail.com>
2017-08-07 01:46:36 -0700

    final monday version

--- Flask_App/views.py
+++ Flask_App/views.py
@@ -10,6 +10,7 @@ from Flask_App import a_Model as mod
 from ast import literal_eval
 
 user = 'ubuntu' #add your username here (same as previous postgreSQL)                      
+#user = 'kimberly'
 host = 'localhost'
 dbname = 'medium'
 db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
